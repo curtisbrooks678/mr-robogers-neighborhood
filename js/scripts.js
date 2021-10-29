a83e0afffaf1b23f1e65ba7714c9542f3287e302
Curtis Brooks <curtisbrooks678@gmail.com>
2021-10-29 10:16:21 -0700

    fifth test passed: It will return a range of 0-9 if the user inputs a number 0-9

--- js/scripts.js
+++ js/scripts.js
@@ -5,21 +5,27 @@ function neighborBot(input) {
   for (i = 0; i < input + 1; i++) {
     inputRange.push(i.toString());
   }
-  return inputRange;
+  inputRange.forEach(function (element) {
+      if (element = "0") {
+        0;
+      } else if (element === "1") {
+        element.replace("1", "Beep!");
+      } else if (element === "2") {
+        "Boop!";
+      } else if (element === "3") {
+        "Won't you be my neighbor?";
+      } else {
+        "error";
+      }
+
+  
 }
-  // inputRange.forEach(function (element) {
-  //   if (element = 0) {
-  //     0;
-  //   } else if (element === 1) {
-  //     "Beep!";
-  //   } else if (element === 2) {
-  //     "Boop!";
-  //   } else if (element === 3) {
-  //     "Won't you be my neighbor?";
-  //   } else {
-  //     "test";
-  //   }
+
 
   // inputRange = [...Array(input + 1).keys()
 
-
+  // let inputRange1 = []
+  // for (i = 0; i < input + 1; i++) {
+  //   inputRange1.push(parseInt(i));
+  // }
+  // console.log(inputRange1);
