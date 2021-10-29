6feef799ee2824219d4217141494af28872b3c25
Curtis Brooks <curtisbrooks678@gmail.com>
2021-10-29 10:54:31 -0700

    sixth test passed: It will return a string for a multi-digit number with rules of most important to least important outlined in assignment prompt

--- js/scripts.js
+++ js/scripts.js
@@ -1,25 +1,45 @@
 
 
+function multDigits(input) {
+  const digitStringArr = input.toString().split("");
+  let digitIntArr = digitStringArr.map(Number);
+  console.log(digitIntArr);
+  finalArr = []
+  digitIntArr.forEach(function (element) {
+    if (element === 1) {
+      finalArr.push("Beep!");
+    } else if (element === 2) {
+      finalArr.push("Boop!");
+    } else if (element === 3) {
+      finalArr.push("Won't you be my neighbor?");
+    } else {
+      finalArr.push(element);
+    }
+  });
+}
+
 function neighborBot(input) { 
   let inputRange = [];
   for (i = 0; i < input + 1; i++) {
-    inputRange.push(i.toString());
+    inputRange.push(i);
   }
+  finalArr = [];
   inputRange.forEach(function (element) {
-      if (element = "0") {
-        0;
-      } else if (element === "1") {
-        element.replace("1", "Beep!");
-      } else if (element === "2") {
-        "Boop!";
-      } else if (element === "3") {
-        "Won't you be my neighbor?";
+      if (element === 0) {
+        finalArr.push("0");
+      } else if (element === 1) {
+        finalArr.push("Beep!");
+      } else if (element === 2) {
+        finalArr.push("Boop!");
+      } else if (element === 3) {
+        finalArr.push("Won't you be my neighbor?");
       } else {
-        "error";
+        finalArr.push(element);
       }
-
-  
+  });
+  alert(finalArr)
 }
+
 
 
   // inputRange = [...Array(input + 1).keys()
