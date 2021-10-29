0c1a73f4d377dd5279bf7e1f475aff4dca5e72ff
Curtis Brooks <curtisbrooks678@gmail.com>
2021-10-29 09:56:02 -0700

    add for loop to change inputRange to range of strings.

--- js/scripts.js
+++ js/scripts.js
@@ -1,14 +1,25 @@
-function neighborBot(input) {
-  if (input === 0) {
-    return 0;
+
+
+function neighborBot(input) { 
+  let inputRange = [];
+  for (i = 0; i < input + 1; i++) {
+    inputRange.push(i.toString());
   }
-  if (input === 1) {
-    return "Beep!";
-  }
-  if (input === 2) {
-    return "Boop!";
-  }
-  if (input === 3) {
-    return "Won't you be my neighbor?";
-  }
+  return inputRange;
 }
+  // inputRange.forEach(function (element) {
+  //   if (element = 0) {
+  //     0;
+  //   } else if (element === 1) {
+  //     "Beep!";
+  //   } else if (element === 2) {
+  //     "Boop!";
+  //   } else if (element === 3) {
+  //     "Won't you be my neighbor?";
+  //   } else {
+  //     "test";
+  //   }
+
+  // inputRange = [...Array(input + 1).keys()
+
+
